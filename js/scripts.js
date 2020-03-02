008e943439725814138983e9a280f5023e1ce5ab
Наймушин Егор <webmaster@farexpo.local>
2020-03-02 17:49:00 +0300

    version 02.03

---
+++ js/scripts.js
@@ -0,0 +1,112 @@
+$(document).ready(function(){
+	$(window).scroll(function(){
+		let scroll = $(this).scrollTop();
+		$('.remont').each(function(){
+			if($(this).offset().top - $(this).outerHeight() <= scroll) {
+				$(this).css('opacity',1);
+			}
+		})
+	});
+})
+
+$(window).load(function() {
+	
+	$('.top-icons').slick({
+	  infinite: true,
+	  autoplay: true,
+	  autoplaySpeed: 1000,
+	  arrows: false,
+	  dots: false,
+	  speed: 1000,
+	  slidesToShow: 11,
+	  slidesToScroll: 3,
+	  responsive: [
+		{
+		  breakpoint: 1600,
+		  settings: {
+			slidesToShow: 9,
+		  }
+		},
+		{
+		  breakpoint: 1199,
+		  settings: {
+			slidesToShow: 7,
+		  }
+		},
+		{
+		  breakpoint: 991,
+		  settings: {
+			slidesToShow: 5,
+		  }
+		},
+		{
+		  breakpoint: 767,
+		  settings: {
+			slidesToShow: 4,
+			slidesToScroll: 2,
+		  }
+		},
+		{
+		  breakpoint: 576,
+		  settings: {
+			slidesToShow: 3,
+			slidesToScroll: 2,
+		  }
+		},
+		{
+		  breakpoint: 480,
+		  settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2,
+		  }
+		}
+	  ] 
+	});
+	
+	$('.bottom-icons').slick({
+	  infinite: true,
+	  autoplay: true,
+	  autoplaySpeed: 1000,
+	  arrows: false,
+	  dots: false,
+	  speed: 1000,
+	  slidesToShow: 8,
+	  slidesToScroll: 3,
+	  responsive: [
+		{
+		  breakpoint: 1199,
+		  settings: {
+			slidesToShow: 7,
+		  }
+		},
+		{
+		  breakpoint: 991,
+		  settings: {
+			slidesToShow: 5,
+		  }
+		},
+		{
+		  breakpoint: 767,
+		  settings: {
+			slidesToShow: 4,
+			slidesToScroll: 2,
+		  }
+		},
+		{
+		  breakpoint: 576,
+		  settings: {
+			slidesToShow: 3,
+			slidesToScroll: 2,
+		  }
+		},
+		{
+		  breakpoint: 480,
+		  settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2,
+		  }
+		}
+	  ] 
+	});
+	
+});
